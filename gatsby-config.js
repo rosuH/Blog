6cfadfea5028741731a72c084145e08096f61b7a
rosuH <hi@rosuh.me>
2022-08-26 10:13:38 +0800

    :arrow_up:

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,15 +83,15 @@ module.exports = {
         feeds: [
           {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map((node) => {
+              return allMarkdownRemark.nodes.map(node => {
                 return Object.assign({}, node.frontmatter, {
                   description: node.excerpt,
                   date: node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + node.fields.slug,
                   guid: site.siteMetadata.siteUrl + node.fields.slug,
                   custom_elements: [{ "content:encoded": node.html }],
-                });
-              });
+                })
+              })
             },
             query: `
               {
@@ -101,8 +101,8 @@ module.exports = {
                   nodes {
                     excerpt
                     html
-                    fields {
-                      slug
+                    fields { 
+                      slug 
                     }
                     frontmatter {
                       title
@@ -113,6 +113,14 @@ module.exports = {
               }
             `,
             output: "/rss.xml",
+            title: "rosu-Blog's RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/blog/",
+            // optional configuration to specify external rss feed, such as feedburner
+            link: "https://feeds.feedburner.com/gatsby/blog",
           },
         ],
       },
