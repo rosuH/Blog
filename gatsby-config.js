6c9bfe1f88676531187c99145c7649864070bf2c
rosuH <hi@rosuh.me>
2023-05-27 10:17:16 +0800

    Remove unnecessary tracking IDs and configuration options from gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -143,17 +143,8 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: [
-          "G-1F0F92B00T", // Google Analytics / GA
-          "", // Google Ads / Adwords / AW
-          "", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          "G-1F0F92B00T"
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
